881f4a702e3453aa638ce6ccf3ed5e97925bc626
kirtimadam <104613258+kirtimadam@users.noreply.github.com>
2023-08-01 15:53:18 +0530

    Added : with switch case

--- src/com/bl/EmployeeWage.java
+++ src/com/bl/EmployeeWage.java
@@ -1,24 +1,29 @@
 package com.bl;
 
 public class EmployeeWage {
+
+   public static final int isFullTime = 2;
+   public static final int isPartTime = 1;
+   public static final int emp_rate_per_hrs = 20;
     public static void main(String[] args) {
        //variables
-        int isFullTime = 2;
-        int isPartTime = 1;
-        int emp_rate_per_hrs = 20;
-        int emphrs;
-        int empwage;
+
+
+         int emphrs =0;
+         int empwage =0;
         //use math random method
         int empcheck = (int)(Math.random()*10%3);
        //check emplyee prsent or not
-        if (empcheck == isFullTime){
-            emphrs = 8;
-        } else if (empcheck == isPartTime) {
 
-            emphrs = 4;
-
-        } else {
-           emphrs = 0;
+        switch (empcheck){
+            case isFullTime:
+                emphrs = 8;
+                break;
+            case isPartTime:
+                emphrs = 4;
+                break;
+            default:
+                System.out.println("not avalable");
         }
 
         empwage = emphrs * emp_rate_per_hrs;
