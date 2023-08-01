888533bc71375e94c220cda98af737a9e08cd79c
kirtimadam <104613258+kirtimadam@users.noreply.github.com>
2023-08-01 15:19:28 +0530

    Added : emplyee attendance

--- src/com/bl/EmployeeWage.java
+++ src/com/bl/EmployeeWage.java
@@ -2,6 +2,15 @@ package com.bl;
 
 public class EmployeeWage {
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation");
-    }
+       //variables
+        int isFullTime = 1;
+        //use math random method
+        int empcheck = (int)(Math.random()*10%2);
+       //check emplyee prsent or not
+        if (empcheck == isFullTime){
+            System.out.println("employee is present");
+        }else {
+            System.out.println("employee not present");
+        }
+          }
 }
