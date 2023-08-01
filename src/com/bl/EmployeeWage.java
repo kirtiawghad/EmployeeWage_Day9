356515232a801c90f90e828de8ee535f620057ac
kirtimadam <104613258+kirtimadam@users.noreply.github.com>
2023-08-01 16:11:12 +0530

    Added : total wage

--- src/com/bl/EmployeeWage.java
+++ src/com/bl/EmployeeWage.java
@@ -5,28 +5,35 @@ public class EmployeeWage {
    public static final int isFullTime = 2;
    public static final int isPartTime = 1;
    public static final int emp_rate_per_hrs = 20;
+   public static final int tital_working_days = 20;
     public static void main(String[] args) {
-       //variables
+        //variables
+        int emphrs = 0;
+        int empwage = 0;
+        int total_wage = 0;
+        for (int i = 0; i <= tital_working_days; i++) {
+            //use math random method
+            int empcheck = (int) (Math.random() * 10 % 3);
+            //check emplyee prsent or not
 
+            switch (empcheck) {
+                case isFullTime:
+                    emphrs = 8;
+                    break;
+                case isPartTime:
+                    emphrs = 4;
+                    break;
+                default:
+                    emphrs = 0;
+            }
 
-         int emphrs =0;
-         int empwage =0;
-        //use math random method
-        int empcheck = (int)(Math.random()*10%3);
-       //check emplyee prsent or not
+            empwage = emphrs * emp_rate_per_hrs;
+            System.out.println("emp wage is : " + empwage);
+            total_wage = total_wage + empwage;
 
-        switch (empcheck){
-            case isFullTime:
-                emphrs = 8;
-                break;
-            case isPartTime:
-                emphrs = 4;
-                break;
-            default:
-                System.out.println("not avalable");
         }
 
-        empwage = emphrs * emp_rate_per_hrs;
-        System.out.println("emp wage is : "+empwage);
-          }
+        System.out.println(" total wage is :"+total_wage);
+
+    }
 }
